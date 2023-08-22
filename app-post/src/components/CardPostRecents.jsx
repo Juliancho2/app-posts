303f95b3d63a57c9e3dd7b338483b9f8d97e0a3a
julian moreno <julianmoreno242@gmail.com>
2023-08-21 22:03:47 -0500

    added className

--- app-post/src/components/CardPostRecents.jsx
+++ app-post/src/components/CardPostRecents.jsx
@@ -6,11 +6,11 @@ import TagItem from './TagItem'
 
 const CardPostRecents = ({post}) => {
     return (
-        <div style={{ display: 'flex' }}>
+        <div className={styles.container_recents} style={{ display: 'flex' }}>
             <div className={styles.container_img}>
                 <img style={{ width: "100%" }} src={'https://posts-c4xu.onrender.com/' + post.cover} alt="" />
             </div>
-            <div className={styles.post_recents_text} style={{ display: 'flex', gap: '8px', flexDirection: 'column', padding: '20px 30px' }}>
+            <div className={styles.post_recents_text} style={{ display: 'flex', gap: '8px', flexDirection: 'column'}}>
                 <small>{post.user.username} | {dateFormatter(post.date)}</small>
                 <h2>{post.title}</h2>
                 <p dangerouslySetInnerHTML={{ __html: truncateString(post.content, 100) }}></p>
